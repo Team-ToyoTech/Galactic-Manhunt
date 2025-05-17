3c8eef08d154121615c9a26fcfaf0f9948c9fca4
DH.L <meozigoon@gmail.com>
2025-05-17 20:20:37 +0900

    v0.1.5-a
    Client class files deleted

--- Client test/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ Client test/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d9f1bb5a8b988098421230084fe4d007285fdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e09ea419c343c6f8e7c78b219569ccd2a64fa53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server test/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
+++ Server test/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d9f1bb5a8b988098421230084fe4d007285fdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0f1495bf46501146b1e0a1c5c76b196329cc06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
